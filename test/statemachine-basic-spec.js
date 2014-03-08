0f09234a3f19591cf30a01c7849589d9c568cb13
hekailiang <hekailiang@gmail.com>
2014-03-08 11:59:30 +0800

    state machine start and isAutoStartEnabled option

--- test/statemachine-basic-spec.js
+++ test/statemachine-basic-spec.js
@@ -2,62 +2,80 @@
 /*jslint node: true */
 'use strict';
 var should = require('chai').should(),
+		expect = require('chai').expect,
     squirrel = require('../index'),
     StateMachine = squirrel.StateMachine;
 
 describe('#StateMachine basic function', function() {
-  it('simple external transition should include old state exit, transition perform and new state entry', 
+	var SimpleStateMachine = StateMachine.extend({
+		// state machine definition
+		definition : {
+	  	initial : "A",
+	  	states : {
+	  		A : { onEntry: "enterA", onExit: "exitA" },
+	  		B : { onEntry: "enterB", onExit: "exitB" }
+	  	},
+
+	  	transitions : [
+	  		{ from : "A", to : "B", on : "A2B", perform : "fromAToB" },
+	  		{ from : "B", to : "A", on : "B2A", perform : function() {this.callSequence += ".fromBToA";} }
+	  	]
+	  },
+
+	  // state machine initialize function
+	  initialize : function() {
+	 		this.callSequence = "";
+	 	},
+
+	 	// state machine actions
+	  enterA : function() {
+	 		this.callSequence += ".enterA";
+	  },
+
+	  exitA : function() {
+	 		this.callSequence += ".exitA";
+	  },
+
+	  fromAToB : function() {
+	  	this.callSequence += ".fromAToB";
+	  },
+
+	  enterB : function() {
+	 		this.callSequence += ".enterB";
+	 	},
+
+  	exitB : function() {
+  		this.callSequence += ".exitB";
+  	}
+	});
+
+	it("A simple state machine should enter its initial states and status should be idle when started", 
+		function() {
+			var stateMachineInstance = new SimpleStateMachine("A");
+			stateMachineInstance.start();
+			stateMachineInstance.callSequence.should.equal(".enterA");
+			stateMachineInstance.getCurrentState().should.equal("A");
+			stateMachineInstance.getStatus().should.equal(squirrel.StateMachineStatus.IDLE);
+	});
+
+	it("A simple state machine should throw error when it is not started and its options 'isAutoStartEnabled' is false", 
+		function() {
+			var fireUnstartedFsmFunc = function() {
+				var stateMachineInstance = new SimpleStateMachine("A", {isAutoStartEnabled : false});
+				stateMachineInstance.fire("A2B");
+			};
+			expect(fireUnstartedFsmFunc).to.throw(/not running/); 
+	});
+
+  it("A simple external transition should include old state exit, transition perform and new state entry", 
   	function() {
-	  	var SimpleStateMachine = StateMachine.extend({
-	  		definition : {
-	  			initial : "A",
-	  			states : {
-	  				A : { onEntry: "enterA", onExit: "exitA" },
-	  				B : { onEntry: "enterB", onExit: "exitB"}
-	  			},
-
-	  			transitions : [
-	  				{from : "A", to : "B", on : "A2B", perform : "fromAToB"},
-	  				{from : "B", to : "A", on : "B2A", perform : "fromBToA"}
-	  			]
-	  		},
-
-	  		initialize : function() {
-	  			this.callSequence = "";
-	  		},
-
-	  		enterA : function() {
-	  			this.callSequence += ".enterA";
-	  		},
-
-	  		exitA : function() {
-	  			this.callSequence += ".exitA";
-	  		},
-
-	  		fromAToB : function() {
-	  			this.callSequence += ".fromAToB";
-	  		},
-
-	  		enterB : function() {
-	  			this.callSequence += ".enterB";
-	  		},
-
-	  		exitB : function() {
-	  			this.callSequence += ".exitB";
-	  		},
-
-	  		fromBToA : function() {
-	  			this.callSequence += ".fromBToA";
-	  		}
-
-	  	}), 
-	  	stateMachineInstance = new SimpleStateMachine("A"), 
+	  	var stateMachineInstance = new SimpleStateMachine(), 
 	  	stateMachineInstance2 = new SimpleStateMachine("B");
 
 	  	stateMachineInstance.fire("A2B");
-	  	stateMachineInstance.callSequence.should.equal(".exitA.fromAToB.enterB");
+	  	stateMachineInstance.callSequence.should.equal(".enterA.exitA.fromAToB.enterB");
 
 	  	stateMachineInstance2.fire("B2A");	  	
-	    stateMachineInstance2.callSequence.should.equal(".exitB.fromBToA.enterA");
+	    stateMachineInstance2.callSequence.should.equal(".enterB.exitB.fromBToA.enterA");
   });
 });
